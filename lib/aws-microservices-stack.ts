c228d819d8c832f379d93e7453cba42bb365d8fd
Sahil Patel <sp0715007@gmail.com>
2022-10-15 03:17:22 -0400

    Created Product Table and Lambda Function using CDK

--- lib/aws-microservices-stack.ts
+++ lib/aws-microservices-stack.ts
@@ -1,16 +1,38 @@
-import * as cdk from 'aws-cdk-lib';
-import { Construct } from 'constructs';
+import * as cdk from "aws-cdk-lib";
+import { RemovalPolicy } from "aws-cdk-lib";
+import { AttributeType, BillingMode, Table } from "aws-cdk-lib/aws-dynamodb";
+import { Runtime } from "aws-cdk-lib/aws-lambda";
+import { NodejsFunction } from "aws-cdk-lib/aws-lambda-nodejs";
+import { Construct } from "constructs";
+import { join } from "path";
 // import * as sqs from 'aws-cdk-lib/aws-sqs';
 
 export class AwsMicroservicesStack extends cdk.Stack {
   constructor(scope: Construct, id: string, props?: cdk.StackProps) {
     super(scope, id, props);
 
-    // The code that defines your stack goes here
+    const productTable = new Table(this, "product", {
+      partitionKey: {
+        name: "id",
+        type: AttributeType.STRING,
+      },
+      tableName: "product",
+      removalPolicy: RemovalPolicy.DESTROY,
+      billingMode: BillingMode.PAY_PER_REQUEST,
+    });
 
-    // example resource
-    // const queue = new sqs.Queue(this, 'AwsMicroservicesQueue', {
-    //   visibilityTimeout: cdk.Duration.seconds(300)
-    // });
+    const productFunction = new NodejsFunction(this, "product", {
+      entry: join(__dirname, "/../src/product/index.js"),
+      bundling: {
+        externalModules: ["aws-sdk"],
+      },
+      environment: {
+        PRIMARY_KEY: "id",
+        DYNAMO_TABLE_NAME: productTable.tableName,
+      },
+      runtime: Runtime.NODEJS_14_X,
+    });
+
+    productTable.grantReadWriteData(productFunction);
   }
 }
